fe984a3210084015bb032abe75b78a2abed20dd2
homuhomy <74242263+homuhomy@users.noreply.github.com>
2023-12-13 11:22:56 +0800

    yes

--- src/core/utils/index.js
+++ src/core/utils/index.js
@@ -1,4 +1,4 @@
 const production = process.env.NODE_ENV === "production"
 
 //change prod-url to the domain later
-export const SITE_URL = production ? 'prod-url' : 'http://localhost:3000'
+export const SITE_URL = production ? 'https://epic-sass-test.vercel.app/' : 'http://localhost:3000'
