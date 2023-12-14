01592004460fbee7973127aa9ccdc3a9004f246d
homuhomy <74242263+homuhomy@users.noreply.github.com>
2023-12-14 12:05:25 +0800

    update some stuff

--- src/login/components/LoginForm.js
+++ src/login/components/LoginForm.js
@@ -1,1 +1,16 @@
-export default function LoginForm() {}
+export default function LoginForm() {
+  return (
+    <form className="content-grid home-hero">
+      <h1>Hi there!</h1>
+      <h3>Enter your e-mail to get the code to login or signup</h3>
+      <div className="email-input">
+        <label htmlFor="email"></label>
+        <input id="email" type="email" autoComplete="email"/>
+        {/* <input id="otp" type=""/> */}
+      </div>
+      <button type="submit" className="large-button">
+        <div className="large-button-text">Login</div>
+      </button>
+    </form>
+  );
+}
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,4 +1,9 @@
 import Link from "next/link";
+import Logo from "src/core/components/Logo";
+import LoginForm from "src/login/components/LoginForm";
+import login from '../../public/assets/login.png';
+import Image from "next/image";
+
 
 // to find the solution for the passwordless, for now it will retrieve the email from stripe
 export default function LoginPage() {
@@ -7,10 +12,16 @@ export default function LoginPage() {
           <div className="border-right bg-offwhite">
             <div className="column-padding">
               <div className="tablet-centered">
-                
+                <Link href="/" className="logo-container">
+                  <Logo style={{width: 150}}/>
+                </Link>
+                <LoginForm />
+
               </div>
             </div>
           </div>
+          <div className="bg-navy border-right"></div>
+          <Image src={login} alt="login" className="callout-image" />
         </div>
       );
 }
