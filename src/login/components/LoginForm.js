f3652fa7ba4d9bc1fb4dd941721dea95bc55175a
homuhomy <74242263+homuhomy@users.noreply.github.com>
2023-12-14 15:11:36 +0800

    testing

--- src/login/components/LoginForm.js
+++ src/login/components/LoginForm.js
@@ -1,13 +1,48 @@
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useState } from "react";
+
 export default function LoginForm() {
+  const supabaseClient = useSupabaseClient();
+  const [error, setError] = useState('');
+  const [isLoading, setLoading] = useState(false);
+  async function onSubmit(event){
+    setLoading(true);
+    event.preventDefault();
+    const email = event.target.elements.email.value;
+    const { error} = await supabaseClient.auth.signInWithOtp({
+      email,
+      options:{
+        shouldCreateUser: false,
+        emailRedirectTo: window.location.origin,
+      }
+    });
+    if(error){
+      setError(error.message);
+      setLoading(false);
+      console.log(error)
+    }else{
+      setError('');
+      setLoading(false);
+    }
+  }
+
   return (
-    <form className="content-grid home-hero">
+    <form onSubmit={onSubmit} className="content-grid home-hero">
+      {error && (
+        <div className="danger" role="alert">
+          {error}
+        </div>
+      )
+
+      }
       <h1>Hi there!</h1>
+      <h5>Enter your e-mail to receive the code to login or sign up</h5>
       <div className="email-input">
         <label htmlFor="email"></label>
         <input id="email" type="email" autoComplete="email"/>
         {/* <input id="otp" type=""/> */}
       </div>
-      <button type="submit" className="large-button">
+      <button disabled={isLoading} type="submit" className="large-button">
         <div className="large-button-text">Login</div>
       </button>
     </form>
