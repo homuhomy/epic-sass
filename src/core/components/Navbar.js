37f4dc071097574ee32e66b70d7e4016acdd2b94
homuhomy <74242263+homuhomy@users.noreply.github.com>
2023-12-18 09:07:09 +0800

    Implemented manage billing

--- src/core/components/Navbar.js
+++ src/core/components/Navbar.js
@@ -1,6 +1,7 @@
 import { useSession, useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
 import Link from "next/link";
 import Logo from "src/core/components/Logo";
+import { SITE_URL } from "../utils";
 
 export default function Navbar() {
   // const user = useUser()
@@ -10,6 +11,14 @@ export default function Navbar() {
 
   function signOut(){
     supabaseClient.auth.signOut()
+  }
+
+  async function onManageBilling(){
+    const response = await fetch(`${SITE_URL}/api/manage-billing`)
+    const data = await response.json()
+    if(data){
+      window.location.href = data.url;
+    }
   }
 
   return (
@@ -22,7 +31,7 @@ export default function Navbar() {
           <Link href="/products" className="nav-link white">
             <div>Products</div>
           </Link>
-          <a className="nav-link border-left white">
+          <a onClick={onManageBilling} className="nav-link border-left white">
             <div>Billing</div>
           </a>
           <div onClick={signOut} className="nav-link black">
@@ -41,4 +50,35 @@ export default function Navbar() {
       )}
     </div>
   );
+
+  // return (
+  //   <div className="nav-container border-b-2 border-black">
+  //     <Link href="/">
+  //       <Logo />
+  //     </Link>
+  //     {session ? (
+  //       <div className="nav-menu">
+  //       <Link href="/login" className="nav-link white">
+  //         <div>Login</div>
+  //       </Link>
+  //       <Link href="/pricing" className="nav-link black">
+  //         <div>Pricing</div>
+  //       </Link>
+  //     </div>
+  //     ) : (
+  //       <div className="nav-menu">
+  //         <Link href="/products" className="nav-link white">
+  //           <div>Products</div>
+  //         </Link>
+  //         <a className="nav-link border-left white">
+  //           <div>Billing</div>
+  //         </a>
+  //         <div onClick={signOut} className="nav-link black">
+  //           <div>Sign out</div>
+  //         </div>
+  //       </div>
+  //     )}
+  //   </div>
+  // );
+  
 }
