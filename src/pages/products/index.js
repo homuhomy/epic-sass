82cf7918aa763fd0c7f888c40091dd9a98bb2b61
homuhomy <74242263+homuhomy@users.noreply.github.com>
2023-12-27 16:09:08 +0800

    pls :(

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,35 +1,35 @@
-import ProductCard from "src/products/components/Card";
-import { supabase } from "supabase";
+// import ProductCard from "src/products/components/Card";
+// import { supabase } from "supabase";
 
-export default function ProductsPage({ products }) {
-  return (
-    <>
-      <div className="section bg-blue">
-        <div className="container">
-          <div className="section-intro">
-            <h1>Latest Products</h1>
-          </div>
-        </div>
-      </div>
+// export default function ProductsPage({ products }) {
+//   return (
+//     <>
+//       <div className="section bg-blue">
+//         <div className="container">
+//           <div className="section-intro">
+//             <h1>Latest Products</h1>
+//           </div>
+//         </div>
+//       </div>
 
-      <div className="section small">
-        <div className="container">
-          <ul className="product-card-grid">
-            {products.map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </ul>
-        </div>
-      </div>
+//       <div className="section small">
+//         <div className="container">
+//           <ul className="product-card-grid">
+//             {products.map((product) => (
+//               <ProductCard key={product.id} product={product} />
+//             ))}
+//           </ul>
+//         </div>
+//       </div>
       
-    </>
-  );
-}
+//     </>
+//   );
+// }
 
-export async function getStaticProps() {
-  let { data: products } = await supabase.from("product").select("*");
+// export async function getStaticProps() {
+//   let { data: products } = await supabase.from("product").select("*");
 
-  return {
-    props: { products },
-  };
-}
+//   return {
+//     props: { products },
+//   };
+// }
