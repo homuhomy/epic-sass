002d3be6aac891ff6d8660ecc6d2264c0c38ea9d
homuhomy <74242263+homuhomy@users.noreply.github.com>
2023-12-27 12:46:08 +0800

    testing otp again...

--- src/login/components/LoginSubmitted.js
+++ src/login/components/LoginSubmitted.js
@@ -15,21 +15,21 @@ import { useRouter } from "next/router";
 
 export default function LoginSubmitted({ submitted }) {
   const supabaseClient = useSupabaseClient();
-  const [otp, setOtp] = useState('');
-  const [error, setError] = useState('');
+  const [otp, setOtp] = useState("");
+  const [error, setError] = useState("");
   const router = useRouter();
 
   async function onVerify() {
     try {
+      // Verify the OTP token
       const { user, error: verifyError } = await supabaseClient.auth.verifyOtp({
-        type: 'signup', 
-        email: submitted,
-        token: otp
+        token_hash: otp,
+        type: "email",
       });
 
       if (verifyError) throw verifyError;
       if (user) {
-        router.push('/success'); 
+        router.push("/success"); // Navigate to success page on successful verification
       }
     } catch (error) {
       setError(error.message);
@@ -38,15 +38,19 @@ export default function LoginSubmitted({ submitted }) {
 
   return (
     <div className="content-grid home-hero">
-      <h1>Link sent!</h1>
-      <p>Check your email ({submitted}) for the OTP code</p>
+      <h1>Check your email</h1>
+      <p>Enter the OTP code sent to {submitted}</p>
       <input
         type="text"
         placeholder="Enter OTP code"
         value={otp}
         onChange={(e) => setOtp(e.target.value)}
       />
-      <button onClick={onVerify} className="large-button"><div className="large-button-text">Verify</div></button>
+      <button onClick={onVerify} className="large-button">
+        <div className="large-button-text">
+          {isLoading ? "Verifying..." : "Verify"}
+        </div>
+      </button>
       {error && <div className="danger">{error}</div>}
     </div>
   );
